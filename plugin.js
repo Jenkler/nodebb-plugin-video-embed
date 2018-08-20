71a145e7adbe2267e6f7a943652911dffd54f5fe
Mikael Jenkler <mikael@jenkler.com>
2018-08-20 20:52:13 +0200

    Added runtime version information Replaced tabs with 2 spaces

--- plugin.js
+++ plugin.js
@@ -7,19 +7,18 @@ youtubeMatch = /<a href.*youtu(?:.*\/v\/|.*v\=|\.be\/)([A-Za-z0-9_\-]*).*<\/a>/g
 youtubeListMatch = /<a href.*youtu(?:.*\/v\/|.*list\=|\.be\/)([A-Za-z0-9_\-]*).*<\/a>/g;
 
 exports.filterParsePost = function(data, callback) {
-	if(!data || !data.postData || !data.postData.content) {
-		return callback(null, data);
-	}
-	if(data.postData.content.match(youtubeListMatch)) {
-		data.postData.content = data.postData.content.replace(youtubeListMatch, youtubeList);
-	}
-	if(data.postData.content.match(youtubeMatch)) {
-		data.postData.content = data.postData.content.replace(youtubeMatch, youtube);
-	}
-	callback(null, data);
+  if(!data || !data.postData || !data.postData.content) {
+    return callback(null, data);
+  }
+  if(data.postData.content.match(youtubeListMatch)) {
+    data.postData.content = data.postData.content.replace(youtubeListMatch, youtubeList);
+  }
+  if(data.postData.content.match(youtubeMatch)) {
+    data.postData.content = data.postData.content.replace(youtubeMatch, youtube);
+  }
+  callback(null, data);
 };
-
 exports.staticAppLoad = function(data, callback) {
-	console.log('Loading Jenkler Video embed plugin');
-	callback();
+  console.log('Loading Jenkler Video embed plugin' + require('./package.json').version);
+  callback();
 };
